598bca988b75249874edee25baa7de22d972f459
Mark Hintz <markohintz@gmail.com>
2017-03-02 03:07:01 +0100

    Initial commit

--- src/DigitalLifeApp.cpp
+++ src/DigitalLifeApp.cpp
@@ -2,33 +2,34 @@
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
 
+#include "Syphon.h"
+
 using namespace ci;
 using namespace ci::app;
-using namespace std;
 
 class DigitalLifeApp : public App {
   public:
+	static void prepareSettings(Settings * settings);
+
 	void setup() override;
-	void mouseDown( MouseEvent event ) override;
 	void update() override;
 	void draw() override;
 };
 
-void DigitalLifeApp::setup()
-{
+void DigitalLifeApp::prepareSettings(Settings * settings) {
+	settings->setTitle("Digital Life");
+	settings->setFullScreen();
+	settings->setHighDensityDisplayEnabled();
 }
 
-void DigitalLifeApp::mouseDown( MouseEvent event )
-{
+void DigitalLifeApp::setup() {
 }
 
-void DigitalLifeApp::update()
-{
+void DigitalLifeApp::update() {
 }
 
-void DigitalLifeApp::draw()
-{
-	gl::clear( Color( 0, 0, 0 ) ); 
+void DigitalLifeApp::draw() {
+	gl::clear(Color(0, 0, 0)); 
 }
 
-CINDER_APP( DigitalLifeApp, RendererGl )
+CINDER_APP(DigitalLifeApp, RendererGl, & DigitalLifeApp::prepareSettings)
