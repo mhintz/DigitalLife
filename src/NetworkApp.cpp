cb13f0872b8345071d2e87142e426f1134212bee
Mark Hintz <markohintz@gmail.com>
2017-05-01 00:15:27 +0200

    Rename a shader because it’ll be used in different parts of the app

--- src/NetworkApp.cpp
+++ src/NetworkApp.cpp
@@ -22,10 +22,10 @@ void NetworkApp::setup()
 	mMatrixBuffer = mOutputCubeFbo->generateCameraMatrixBuffer();
 	mMatrixBuffer->bindBufferBase(matrixBindingPoint);
 
-	mRenderLinesToCubeMap = gl::GlslProg::create(app::loadAsset("DLRenderIntoCubeMap_v.glsl"), app::loadAsset("NTRenderIntoCubeMap_f.glsl"), app::loadAsset("DLRenderIntoCubeMap_lines_g.glsl"));
+	mRenderLinesToCubeMap = gl::GlslProg::create(app::loadAsset("DLRenderIntoCubeMap_v.glsl"), app::loadAsset("DLRenderIntoCubeMap_f.glsl"), app::loadAsset("DLRenderIntoCubeMap_lines_g.glsl"));
 	mRenderLinesToCubeMap->uniformBlock("uMatrices", matrixBindingPoint);
 
-	mRenderPointsToCubeMap = gl::GlslProg::create(app::loadAsset("DLRenderIntoCubeMap_v.glsl"), app::loadAsset("NTRenderIntoCubeMap_f.glsl"), app::loadAsset("DLRenderIntoCubeMap_points_g.glsl"));
+	mRenderPointsToCubeMap = gl::GlslProg::create(app::loadAsset("DLRenderIntoCubeMap_v.glsl"), app::loadAsset("DLRenderIntoCubeMap_f.glsl"), app::loadAsset("DLRenderIntoCubeMap_points_g.glsl"));
 	mRenderPointsToCubeMap->uniformBlock("uMatrices", matrixBindingPoint);
 
 	// Set up the simulation data
