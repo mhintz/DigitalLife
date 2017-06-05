f202b31fc358980bf43921bb72f345ffff608974
Mark Hintz <markohintz@gmail.com>
2017-06-05 20:28:46 +0200

    Add a long fade at the beginning too

--- src/DigitalLifeApp.cpp
+++ src/DigitalLifeApp.cpp
@@ -137,7 +137,7 @@ void DigitalLifeApp::setup() {
 	double const narration_duration = secFromHMS(0.0, 5.0, 38.5); // the track is 5:38.5 long at the moment
 
 	double const fade_dur = 0.5;
-	double const last_fade_dur = 2.0;
+	double const end_fade_dur = 1.5;
 
 	mPlaybackTimeline.apply(& mPlaybackProgress)
 		.then<choreograph::RampTo>(narration_duration, narration_duration)
@@ -145,8 +145,8 @@ void DigitalLifeApp::setup() {
 		.finishFn([&] { mNarrationPlayer->stop(); mPlaybackTimeline.resetTime(); });
 
 	mPlaybackTimeline.apply(& mFrameAlpha)
-		.then<choreograph::RampTo>(1.0f, fade_dur)
-		.then<choreograph::Hold>(1.0f, D1_start - I1_start - 2.0 * fade_dur) // Hold for the entire intro - both I1 and I2
+		.then<choreograph::RampTo>(1.0f, end_fade_dur)
+		.then<choreograph::Hold>(1.0f, D1_start - I1_start - end_fade_dur - fade_dur) // Hold for the entire intro - both I1 and I2
 		.then<choreograph::RampTo>(0.0f, fade_dur)
 		.then<choreograph::RampTo>(1.0f, fade_dur)
 		.then<choreograph::Hold>(1.0f, D2_start - D1_start - 2.0 * fade_dur) // D1
@@ -161,8 +161,8 @@ void DigitalLifeApp::setup() {
 		.then<choreograph::Hold>(1.0f, N1_start - F1_start - 2.0 * fade_dur) // F1
 		.then<choreograph::RampTo>(0.0f, fade_dur)
 		.then<choreograph::RampTo>(1.0f, fade_dur)
-		.then<choreograph::Hold>(1.0f, narration_duration - N1_start - fade_dur - last_fade_dur) // N1
-		.then<choreograph::RampTo>(0.0f, last_fade_dur); // Long fade to black at end
+		.then<choreograph::Hold>(1.0f, narration_duration - N1_start - fade_dur - end_fade_dur) // N1
+		.then<choreograph::RampTo>(0.0f, end_fade_dur); // Long fade to black at end
 
 	mPlaybackTimeline.cue([&] { mActiveAppType = AppType::REACTION_DIFFUSION; }, I1_start);
 	mPlaybackTimeline.cue([&] { mActiveAppType = AppType::REACTION_DIFFUSION; }, I2_start);
